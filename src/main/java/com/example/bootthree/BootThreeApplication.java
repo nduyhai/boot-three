d2d981f8d7fc511f07006f259f1606ecc992cb7d
Hai Nguyen <haind.vng@gmail.com>
2022-11-19 10:22:34 +0700

    first commit

---
+++ src/main/java/com/example/bootthree/BootThreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootThreeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BootThreeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/bootthree/BootThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootthree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootThreeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
